56587d1c9120364b1708c686a6eda9ad1bca23cf
saurav pawar <pawarsaurav957@gmail.com>
2026-04-20 14:11:19 +0530

    playing with the routes

--- src/app.js
+++ src/app.js
@@ -4,21 +4,10 @@ const express = require("express");
 const app = express();
 
 //This will only handles GET call to /user
-app.get("/user",(req,res)=>{
+app.get("/user/:userId/:name",(req,res)=>{
+    console.log(req.query);
+    console.log(req.params);
     res.send({firstname:"saurav",lastname: "pawar"});
-})
-
-app.post("/user",(req,res)=>{
-    //saving data to DB
-    res.send("Data saved succesfully!");
-})
-
-app.delete("/user",(req,res)=>{
-    res.send("Data delete succesfully!");
-})
-
-app.patch("/user",(req,res)=>{
-    res.send("Data patch succesfully!");
 })
 
 //use :- will match all the HTTP methods api call to /test
