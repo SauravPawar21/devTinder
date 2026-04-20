1b3ed486d42ce72a08dc148f5cf6466b5e8c8608
saurav pawar <pawarsaurav957@gmail.com>
2026-04-20 15:55:38 +0530

    Multiple-route-handlers

--- src/app.js
+++ src/app.js
@@ -3,12 +3,28 @@ const express = require("express");
 //creating an new express application
 const app = express();
 
-//This will only handles GET call to /user
-app.get("/user/:userId/:name",(req,res)=>{
-    console.log(req.query);
-    console.log(req.params);
-    res.send({firstname:"saurav",lastname: "pawar"});
-})
+
+app.get(
+    "/user",
+    (req,res,next)=>{
+    console.log("1 Route ");
+    next();
+},
+    (req,res,next)=>{
+    console.log("2 Route ");
+    // res.send("2 Response");
+    next()
+},
+    (req,res,next)=>{
+    console.log("3 Route ");
+    // res.send("3 Response");
+    next()
+},
+    (req,res,next)=>{
+    console.log("4 Route ");
+    res.send("4 Response");
+}
+)
 
 //use :- will match all the HTTP methods api call to /test
 app.use("/test",(req,res)=>{
