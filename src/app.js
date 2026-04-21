24dd50f9082d331c31a17d0a9516e129ba2df98f
saurav pawar <pawarsaurav957@gmail.com>
2026-04-21 23:47:02 +0530

    dynamic-data-in-signup-api

--- src/app.js
+++ src/app.js
@@ -3,14 +3,18 @@ const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
 
+app.use(express.json());
+
 app.post("/signUp", async (req, res) => {
   //creating an new instance of the user modle
-  const user = new User({
-    firstName: "virat",
-    lastName: "kohli",
-    emailId: "virat@kohli.com",
-    password: "virat@213",
-  });
+  
+  const user = new User(req.body);
+//   const user = new User({
+//     firstName: "virat",
+//     lastName: "kohli",
+//     emailId: "virat@kohli.com",
+//     password: "virat@213",
+//   });
   try {
      await user.save();
   res.send("User Added successfully");
