4f6051ec666a99c6502c80c84568ecdb28b825ea
saurav pawar <pawarsaurav957@gmail.com>
2026-04-23 13:41:42 +0530

    added-an-encrypt-password-fuctionality

--- src/app.js
+++ src/app.js
@@ -2,12 +2,28 @@ const express = require("express");
 const connectDB = require("./config/database");
 const app = express();
 const User = require("./models/user");
+const { validateSignUpData } = require("./utils/validation");
+const bcrypt = require("bcrypt");
 
 app.use(express.json());
 
 app.post("/signUp", async (req, res) => {
-  const user = new User(req.body);
   try {
+    //validation of data
+    validateSignUpData(req);
+
+    const { firstName,lastName,emailId, password } = req.body;
+    //Encrypt the password
+    const passwordHash = await bcrypt.hash(password,10);
+    console.log(passwordHash);
+
+    //creating a new instance of the user model
+    const user = new User({
+      firstName,
+      lastName,
+      emailId, 
+      password: passwordHash,
+    });
      await user.save();
   res.send("User Added successfully");
   } catch (err) {
---
+++ src/utils/validation.js
@@ -0,0 +1,17 @@
+const validator = require("validator");
+const validateSignUpData = (req) => {
+  const { firstName, lastName, emailId, password } = req.body;
+
+  if (!firstName || !lastName) {
+    throw new Error("Name is not valid");
+  } else if (!validator.isEmail(emailId)) {
+    throw new Error("Invalid Email ID");
+  } else if (!validator.isStrongPassword(password)) {
+    throw new Error("Please Enter an Strong password");
+  }
+};
+
+
+module.exports={
+    validateSignUpData,
+}
